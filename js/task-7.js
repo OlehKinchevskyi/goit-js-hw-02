b045c4b5a067f5ae7cc9f2c9027c324711e29109
Oleh Kinchevskyi <Oleh.Kinchevskyi@gmail.com>
2020-08-29 08:00:19 +0300

    Додає завдання 9

--- js/task-7.js
+++ js/task-7.js
@@ -5,21 +5,14 @@ function filterArray(array) {
   const numbers = [];
   for (let i = 0; i < array.length; i += 1) {
     // Write code under this line
-    function filterArray(array) {
-      'use strict';
-      const numbers = [];
-      for (let i = 0; i < array.length; i += 1) {
-        // Write code under this line
-        if (Number.isFinite(array[i])) {
-          numbers.push(array[i]);
-        }
-      }
-      return numbers;
+    if (Number.isFinite(array[i])) {
+      numbers.push(array[i]);
     }
+    return numbers;
   }
+
   return numbers;
 }
-
 console.log(filterArray([-2, 0, 2]));
 // [-2, 0, 2]
 
